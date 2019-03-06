5197ff8be6082b13810aeedb28ae7bbe3c338ad1
wovelscotch <wolvelscotch@yahoo.com>
2019-03-05 20:35:49 -0600

    update processor.h
    -added process_list_size var with sets/gets

--- processor.h
+++ processor.h
@@ -9,6 +9,7 @@ class Processor
 		int cycle_count;		//sum of cycles in process_list
 		Process ** process_list;	//list of assigned processes
 		int process_count;		//amount of processes in list
+		int process_list_size;		//size of list, including vacancies 
 	public:
 		//constructors
 		Processor();
@@ -26,6 +27,9 @@ class Processor
 		
 		void setProcessCount(int inp){process_count = inp;}
 		int getProcessCount(){return process_count;}
+	
+		void setProcessListSize(int inp){process_list_size = inp;}
+		int getProcessListSize(){return process_list_size;}
 		
 		void addProcess(Process*);
 		Process * getProcess(int);
